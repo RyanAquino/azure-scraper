9e99b83f7c2d647eb2657d3ac839df41e737001c
Heinrich P. Bangui <heinrichbangui@gmail.com>
2023-04-25 04:26:08 +0800

    Fixed child clicking recursion.

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -8,25 +8,35 @@ import config
 
 
 def click_button_by_id(driver, id):
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, id)))
-    driver.find_element(By.ID, id).click()
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, id)))
+    element.click()
 
 
 def click_button_by_xpath(driver, xpath):
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-    driver.find_element(By.XPATH, xpath).click()
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+    element.click()
+
+
+def click_button_by_tag(driver, tag):
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, tag)))
+    element.click()
 
 
 def send_keys_by_name(driver, name, keys):
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, name)))
-    driver.find_element(By.NAME, name).send_keys(keys)
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, name)))
+    element.send_keys(keys)
 
 
 def find_elements_by_xpath(driver, xpath):
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_all_elements_located((By.XPATH, xpath))
-    )
-    return driver.find_elements(By.XPATH, xpath)
+    try:
+        e = WebDriverWait(driver, 10).until(
+            EC.visibility_of_all_elements_located((By.XPATH, xpath))
+        )
+    except Exception as e:
+        return None
+
+    return e
+
 
 
 def login(driver, email, password):
@@ -37,47 +47,50 @@ def login(driver, email, password):
     click_button_by_id(driver, "idSIButton9")
 
 
-def scrape_work_items(driver):
+def scrape_work_items(driver, e):
     # Extract the data you need from the current element
 
     result = {
-        "id": driver.find_element(By.XPATH, "//span[@aria-label='ID Field']").text,
-        "title": driver.find_element(
+        "id": e.find_element(By.XPATH, "//span[@aria-label='ID Field']").text,
+        "title": e.find_element(
             By.XPATH, "//input[@aria-label='Title Field']"
         ).get_attribute("value"),
         "children": [],
     }
 
     child_xpath = (
-        "//div[child::div[contains(@class, 'la-group-title') "
+        ".//div[child::div[contains(@class, 'la-group-title') "
         "and contains(text(), 'Child')]]//div[@class='la-item']"
     )
 
-    child_elements = find_elements_by_xpath(driver, child_xpath)
+    child_elements = find_elements_by_xpath(e, child_xpath)
 
     if child_elements:
         for child in child_elements:
+            task = child.find_element(By.XPATH, ".//a")
+            child_id = task.get_attribute("href").split("/")[-1]
             result["children"].append(
                 {
-                    "id": child.find_element(By.XPATH, child_xpath + "//a")
-                    .get_attribute("href")
-                    .split("/")[-1],
-                    "title": child.find_element(By.XPATH, child_xpath + "//a").text,
+                    "id": child_id,
+                    "title": task.text,
                     "children": [],
                 }
             )
 
-    # TODO: Fix login on recursion
-    # dialog_xpath = "//div[contains(@tabindex, '-1') and contains(@role, 'dialog') and contains(@style, '10006')]"
-    # click_button_by_xpath(child, child_xpath + "//a")
-    # child_dialog_box = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, dialog_xpath)))
-    #
-    # scrape_work_items(child_dialog_box)
-    #
-    # click_button_by_xpath(child_dialog_box, ".//button[contains(@class, 'ui-button')]")
+            task.click()
+            time.sleep(5)
 
-    # else:
-    #     click_button_by_xpath(driver, ".//button[contains(@class, 'ui-button')]")
+            dialog_xpath = f"//span[@aria-label='ID Field' and " \
+                           f"contains(text(), '{child_id}')]//ancestor::div"
+
+            child_dialog_box = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.XPATH, dialog_xpath))
+            )
+            scrape_work_items(driver, child_dialog_box)
+            # TODO: Fix the closing of dialog boxes
+            click_button_by_xpath(child_dialog_box, ".//button[contains(@class, 'ui-button')]")
+    else:
+        click_button_by_xpath(e, ".//button[contains(@class, 'ui-button')]")
 
     return result
 
@@ -104,12 +117,13 @@ def main(driver, url, email, password):
             EC.presence_of_element_located((By.XPATH, dialog_xpath))
         )
 
-        result_set.append(scrape_work_items(dialog_box))
+        result = scrape_work_items(driver, dialog_box)
+        result_set.append(result)
 
         click_button_by_xpath(dialog_box, ".//button[contains(@class, 'ui-button')]")
 
-    print(result_set)
-
+    for result in result_set:
+        print(result)
 
 if __name__ == "__main__":
     chrome_options = ChromeOptions()
