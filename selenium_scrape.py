eb4c4c007f6b314399d75422c62b8fede727ef2c
Heinrich P. Bangui <heinrichbangui@gmail.com>
2023-04-26 00:47:16 +0800

    Fix closing of buttons

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -13,17 +13,23 @@ def click_button_by_id(driver, id):
 
 
 def click_button_by_xpath(driver, xpath):
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, xpath))
+    )
     element.click()
 
 
 def click_button_by_tag(driver, tag):
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.TAG_NAME, tag)))
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.TAG_NAME, tag))
+    )
     element.click()
 
 
 def send_keys_by_name(driver, name, keys):
-    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, name)))
+    element = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, name))
+    )
     element.send_keys(keys)
 
 
@@ -38,6 +44,16 @@ def find_elements_by_xpath(driver, xpath):
     return e
 
 
+def find_element_by_xpath(driver, xpath):
+    try:
+        e = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpath))
+        )
+    except Exception as e:
+        return None
+
+    return e
+
 
 def login(driver, email, password):
     send_keys_by_name(driver, "loginfmt", email)
@@ -47,83 +63,68 @@ def login(driver, email, password):
     click_button_by_id(driver, "idSIButton9")
 
 
-def scrape_work_items(driver, e):
-    # Extract the data you need from the current element
-
-    result = {
-        "id": e.find_element(By.XPATH, "//span[@aria-label='ID Field']").text,
-        "title": e.find_element(
-            By.XPATH, "//input[@aria-label='Title Field']"
-        ).get_attribute("value"),
-        "children": [],
-    }
-
+def scrape_child_work_items(driver, dialog_box, data):
     child_xpath = (
         ".//div[child::div[contains(@class, 'la-group-title') "
         "and contains(text(), 'Child')]]//div[@class='la-item']"
     )
 
-    child_elements = find_elements_by_xpath(e, child_xpath)
+    child_work_items = find_elements_by_xpath(dialog_box, child_xpath)
 
-    if child_elements:
-        for child in child_elements:
-            task = child.find_element(By.XPATH, ".//a")
-            child_id = task.get_attribute("href").split("/")[-1]
-            result["children"].append(
-                {
-                    "id": child_id,
-                    "title": task.text,
-                    "children": [],
-                }
-            )
+    if child_work_items:
+        for work_item in child_work_items:
+            work_item_element = find_element_by_xpath(work_item, ".//a")
+            child_id = work_item_element.get_attribute("href").split("/")[-1]
+            task = work_item_element.text
 
-            task.click()
+            print(task, child_id)
+
+            work_item_element.click()
             time.sleep(5)
 
-            dialog_xpath = f"//span[@aria-label='ID Field' and " \
-                           f"contains(text(), '{child_id}')]//ancestor::div"
-
-            child_dialog_box = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.XPATH, dialog_xpath))
+            dialog_xpath = (
+                f"//span[@aria-label='ID Field' and "
+                f"contains(text(), '{child_id}')]//ancestor::div"
             )
-            scrape_work_items(driver, child_dialog_box)
-            # TODO: Fix the closing of dialog boxes
-            click_button_by_xpath(child_dialog_box, ".//button[contains(@class, 'ui-button')]")
-    else:
-        click_button_by_xpath(e, ".//button[contains(@class, 'ui-button')]")
 
-    return result
+            child_dialog_box = find_element_by_xpath(driver, dialog_xpath)
+            scrape_child_work_items(driver, child_dialog_box, data)
+
+            click_button_by_xpath(
+                child_dialog_box, ".//button[contains(@class, 'ui-button')]"
+            )
 
 
 def main(driver, url, email, password):
-    result_set = []
-
-    # Navigate to site
+    # Navigate to the site and login
     driver.get(url)
-    wait = WebDriverWait(driver, 20)
-
-    # Login
     login(driver, email, password)
 
-    # Top Level Elements
-    top_level_elements = find_elements_by_xpath(driver, '//div[@aria-level="1"]')
+    # Find each work item
+    work_items = find_elements_by_xpath(driver, '//div[@aria-level="1"]')
 
-    for top_level_element in top_level_elements:
-        click_button_by_xpath(top_level_element, ".//a")
+    data = {}
+    for work_item in work_items:
         time.sleep(5)
 
+        work_item_element = find_element_by_xpath(work_item, ".//a")
+
+        work_item_id = work_item_element.get_attribute("href").split("/")[-1]
+        title = work_item_element.text
+
+        data[work_item_id] = {"title": title}
+
+        # Click
+        work_item_element.click()
         dialog_xpath = "//div[contains(@tabindex, '-1') and contains(@role, 'dialog')]"
-        dialog_box = wait.until(
-            EC.presence_of_element_located((By.XPATH, dialog_xpath))
-        )
+        dialog_box = find_element_by_xpath(work_item, dialog_xpath)
 
-        result = scrape_work_items(driver, dialog_box)
-        result_set.append(result)
+        # Scrape Child Items
+        scrape_child_work_items(driver, dialog_box, data)
 
+        # Close dialog box
         click_button_by_xpath(dialog_box, ".//button[contains(@class, 'ui-button')]")
 
-    for result in result_set:
-        print(result)
 
 if __name__ == "__main__":
     chrome_options = ChromeOptions()
