a7e087c1b1786976f42aaee90f6236edad13dfd6
ryanaquino <ryan@extrac.io>
2023-04-30 16:33:10 +0800

    milestone-5: config cleanup

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 URL = "https://dev.azure.com/nvtmsovybqhxqzgyzf/scrum/_backlogs/backlog/scrum%20Team/Epics/"
-EMAIL = "zumhxnwvstlnqvarxa@bbitq.com"
-PASSWORD = "7aa2|id?ly3_4*{4U3^w"
+EMAIL = ""
+PASSWORD = ""
