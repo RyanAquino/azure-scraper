db70934bce431ef5a972f836c93f46ef65bd73de
RyanAquino <ryan.aquino7110@gmail.com>
2023-06-18 15:19:10 +0800

    TSK-6: refactor

---
+++ action_utils.py
@@ -0,0 +1,83 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+import config
+
+
+def click_button_by_id(driver, element_id):
+    element = WebDriverWait(driver, config.MAX_WAIT_TIME).until(
+        EC.element_to_be_clickable((By.ID, element_id))
+    )
+    element.click()
+
+
+def click_button_by_xpath(driver, xpath):
+    element = WebDriverWait(driver, config.MAX_WAIT_TIME).until(
+        EC.element_to_be_clickable((By.XPATH, xpath))
+    )
+    element.click()
+
+
+def send_keys_by_name(driver, name, keys):
+    element = WebDriverWait(driver, config.MAX_WAIT_TIME).until(
+        EC.element_to_be_clickable((By.NAME, name))
+    )
+    element.send_keys(keys)
+
+
+def find_elements_by_xpath(driver, xpath):
+    try:
+        e = WebDriverWait(driver, config.MAX_WAIT_TIME).until(
+            EC.visibility_of_all_elements_located((By.XPATH, xpath))
+        )
+    except Exception as e:
+        return None
+
+    return e
+
+
+def find_element_by_xpath(driver, xpath):
+    try:
+        e = WebDriverWait(driver, config.MAX_WAIT_TIME).until(
+            EC.element_to_be_clickable((By.XPATH, xpath))
+        )
+    except Exception as e:
+        return None
+
+    return e
+
+
+def get_input_value(driver, xpath):
+    if element := find_element_by_xpath(driver, xpath):
+        return element.get_attribute("value")
+
+
+def get_anchor_link(driver, xpath):
+    if element := find_element_by_xpath(driver, xpath):
+        return element.get_attribute("href")
+
+
+def get_text(driver, xpath):
+    if element := find_element_by_xpath(driver, xpath):
+        return element.text
+
+
+def add_line_break(word, max_length):
+    if len(word) > max_length:
+        return (
+            word[:max_length]
+            + "\n           "
+            + add_line_break(word[max_length:], max_length)
+        )
+    else:
+        return word
+
+
+def expand_collapsed_by_xpath(dialog_box):
+    collapsed_xpath = ".//div[@aria-expanded='false']"
+    collapsed = find_elements_by_xpath(dialog_box, collapsed_xpath)
+
+    if collapsed:
+        for collapse_item in collapsed:
+            collapse_item.click()
---
+++ driver_utils.py
@@ -0,0 +1,62 @@
+import os
+import platform
+import shutil
+from pathlib import Path
+from sys import platform
+
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service
+
+import config
+from logger import logging
+
+
+def get_driver_by_os():
+    ps = platform.system()
+
+    if ps == "Windows":
+        driver_path = "chromedriver.exe"
+    elif ps == "Darwin":
+        driver_path = "chromedriver_mac"
+
+        if platform.processor() == "arm":
+            driver_path = "chromedriver_mac_arm"
+    else:
+        driver_path = "chromedriver_linux"
+
+    logging.info(f"Using driver {driver_path}.")
+
+    return Service(executable_path=f"drivers/{driver_path}")
+
+
+def chrome_settings_init():
+    download_directory = Path(f"{os.getcwd()}/data/attachments")
+    language_code = "en-GB"
+
+    chrome_options = ChromeOptions()
+    # chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_experimental_option(
+        "prefs",
+        {
+            "download.default_directory": str(download_directory),
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "intl.accept_languages": language_code,
+        },
+    )
+
+    chrome_options.add_experimental_option("detach", True)
+    chrome_settings = {"options": chrome_options}
+
+    if config.BINARY_PATH_LOCATION:
+        chrome_options.binary_location = config.BINARY_PATH_LOCATION
+    else:
+        chrome_settings["service"] = get_driver_by_os()
+
+    # Clean attachments directory
+    if os.path.isdir(download_directory):
+        shutil.rmtree(download_directory)
+        os.makedirs(download_directory)
+
+    return chrome_settings, download_directory
