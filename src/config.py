1c5f62c9ea133d413cbd791fdd4df77f3f244395
ryanaquino <ryan@extrac.io>
2023-06-18 15:15:56 +0800

    milestone-5: creds cleanup

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@ Set scrape configurations
 """
 BASE_URL = "https://dev.azure.com/nvtmsovybqhxqzgyzf/"
 BACKLOG_ENDPOINT = "scrum/_backlogs/backlog/scrum%20Team/Epics/"
-EMAIL = "zumhxnwvstlnqvarxa@bbitq.com"
-PASSWORD = "7aa2|id?ly3_4*{4U3^w"
+EMAIL = ""
+PASSWORD = ""
 BINARY_PATH_LOCATION = "/Applications/Chromium.app/Contents/MacOS/Chromium"
 WORK_ITEM_ENDPOINT = "scrum/_workitems/edit/"
 MAX_RETRIES = 100
