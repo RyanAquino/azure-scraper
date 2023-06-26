5a9c63da9b2b996f3c9d165bd6ca992f4172f5fe
paul <paul@localhost>
2023-06-25 20:51:54 -0400

    config to dot env

--- src/config.py
+++ src/config.py
@@ -1,11 +1,16 @@
 """
 Set scrape configurations
 """
-BASE_URL = "https://dev.azure.com/nvtmsovybqhxqzgyzf/"
-BACKLOG_ENDPOINT = "scrum/_backlogs/backlog/scrum%20Team/Epics/"
-EMAIL = ""
-PASSWORD = ""
-BINARY_PATH_LOCATION = "/Applications/Chromium.app/Contents/MacOS/Chromium"
-WORK_ITEM_ENDPOINT = "scrum/_workitems/edit/"
-MAX_RETRIES = 100
-MAX_WAIT_TIME = 10
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+BASE_URL = os.getenv('base_url')
+BACKLOG_ENDPOINT = os.getenv('backlog_endpoint')
+EMAIL = os.getenv('email')
+PASSWORD = os.getenv('password')
+BINARY_PATH_LOCATION = os.getenv('binary_path_location')
+WORK_ITEM_ENDPOINT = os.getenv('work_item_endpoint')
+MAX_RETRIES = os.getenv('max_retries')
+MAX_WAIT_TIME = os.getenv('max_wait_time')
