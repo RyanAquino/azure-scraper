ba612d8c8f78f20cf48b1bafbf484b467c269ca7
heinrichb <heinrichbangui@gmail.com>
2023-07-16 23:25:46 -0700

    TSK-14: Separate unlink of symbolic links and delete empty folders

--- scripts/symlink_tool_1.py
+++ scripts/symlink_tool_1.py
@@ -5,22 +5,38 @@
 from pathlib import Path
 
 
-def traverse_directory(directory):
-    for item in directory.glob("*"):
+def traverse_directory(directory_path):
+    for item_path in directory_path:
+        yield item_path
+
+
+def unlink_all_symlinks(directory):
+    for item in traverse_directory(directory):
+        if item.is_symlink():
+            print(f"Unlink item {item}.")
+            item.unlink()
+        else:
+            print(f"Skip!! item {item} is not a symlink.")
+
+
+def remove_empty_directories(directory):
+    for item in traverse_directory(directory):
         if item.is_dir():
             contain_files = any(item.iterdir())
 
             if contain_files is False:
-                item.rmdir()
-                print(f"Remove directory {item}")
+                print(item.rmdir())
+                print(f"Remove directory {item}.")
+            else:
+                print(f"Skip!! directory {item} contains file.")
 
-            traverse_directory(item)
 
-        elif item.is_symlink():
-            item.unlink()
-            print(f"Removing symbolic link: {item}")
+def main(path):
+    directory = Path(path)
+    unlink_all_symlinks(directory.glob("**/*"))
+    remove_empty_directories(sorted(directory.glob("**/*"), reverse=True))
 
 
 if __name__ == "__main__":
-    basepath = Path("sample_directory")
-    traverse_directory(basepath)
+    basepath = "sample_directory"
+    main(basepath)
