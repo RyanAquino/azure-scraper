630119010b39c2160e48cca30ee029de2ff8b9e6
heinrichb <heinrichbangui@gmail.com>
2023-07-16 21:49:14 -0700

    TSK 14: Create script to unlink symlinks and delete empty folders

---
+++ scripts/create_symlinks_test.py
@@ -0,0 +1,26 @@
+""" Create sample nested directories with symlink
+"""
+from pathlib import Path
+import platform
+import os
+
+
+def create_nested_directories(path, link, depth=5):
+    if depth > 0:
+        path = Path(path, f"directory_{depth}")
+        path.mkdir(parents=True, exist_ok=True)
+
+        file_path1 = Path(path, "symlink_file1")
+        file_path2 = Path(path, "symlink_file2")
+        file_path3 = Path(path, "symlink_file3")
+        os.symlink(file_path1, link)
+        os.symlink(file_path2, link)
+        os.symlink(file_path3, link)
+
+        create_nested_directories(path, link, depth=depth - 1)
+
+
+if __name__ == "__main__":
+    base_path = Path("sample_directory")
+    symlink = Path("link_here", "test")
+    create_nested_directories(base_path, symlink)
