b290ce58f0d4ffd68567bca71c540e7390b6eac1
RyanAquino <ryan.aquino7110@gmail.com>
2024-04-07 00:00:18 +0800

    milestone-23: Fixes per folder/changeset

--- src/changeset_scraper.py
+++ src/changeset_scraper.py
@@ -1,18 +1,21 @@
+import glob
 import os
+import re
 import shutil
 import time
+import traceback
 from pathlib import Path
 from zipfile import ZipFile
 
 from selenium import webdriver
 
 import config
-from action_utils import find_element_by_xpath, find_elements_by_xpath
+from action_utils import find_element_by_xpath, find_elements_by_xpath, get_text
 from driver_utils import chrome_settings_init
 from main import login
 
 
-def download_changeset(driver):
+def download_changeset(driver, changeset_downloads, downloaded_ctr):
     browse_files = find_element_by_xpath(driver, "//a[@id='__bolt-browse-files']")
     driver.execute_script("arguments[0].click();", browse_files)
 
@@ -21,6 +24,8 @@ def download_changeset(driver):
 
     download_btn = find_element_by_xpath(driver, "//div[@id='__bolt-download-text']")
     driver.execute_script("arguments[0].click();", download_btn)
+
+    return wait_for_download(changeset_downloads, downloaded_ctr)
 
 
 def wait_for_download(chrome_downloads, downloaded_ctr):
@@ -48,7 +53,9 @@ def wait_for_download(chrome_downloads, downloaded_ctr):
     else:
         print("File downloaded")
 
-    return files
+    latest_file = Path(chrome_downloads, max(files, key=lambda f: f.stat().st_mtime))
+
+    return latest_file
 
 
 def get_changeset_urls(driver):
@@ -64,6 +71,41 @@ def get_changeset_urls(driver):
     return changeset_urls
 
 
+def get_valid_paths(driver):
+    files_changed = find_elements_by_xpath(driver, "//tr[@role='treeitem']") or []
+    valid_file_paths = set()
+
+    for file in files_changed:
+        driver.execute_script("arguments[0].click();", file)
+        path = find_element_by_xpath(
+            driver, "//span[@role='heading']/parent::span/following-sibling::span"
+        )
+
+        header_xpath = "//span[@role='heading']"
+        file_name = get_text(driver, header_xpath)
+
+        pattern = r"^\d+ changed files?$"
+        match = re.match(pattern, file_name)
+
+        if not path or match:
+            continue
+
+        path = "/".join(path.text.split("/")[1:])
+        valid_file_paths.add(Path(path))
+
+    return valid_file_paths
+
+
+def clean_extract(latest_file, _id):
+    extract_path = Path(latest_file.parent.parent, "changesets", _id)
+    with ZipFile(latest_file, "r") as zObject:
+        zObject.extractall(path=extract_path)
+
+    os.remove(latest_file)
+
+    return extract_path
+
+
 def scrape_changeset(driver, changeset_downloads):
     files = list(changeset_downloads.iterdir())
     downloaded_ctr = len(files) + 1
@@ -71,19 +113,40 @@ def scrape_changeset(driver, changeset_downloads):
 
     for changeset_url in changeset_urls:
         driver.get(changeset_url)
-        download_changeset(driver)
-        files = wait_for_download(changeset_downloads, downloaded_ctr)
-        latest_file = Path(
-            changeset_downloads, max(files, key=lambda f: f.stat().st_mtime)
-        )
+        _id = changeset_url.split("/")[-2]
 
-        with ZipFile(latest_file, "r") as zObject:
-            zObject.extractall(path=Path(latest_file.parent.parent, "changesets"))
-
-        os.remove(latest_file)
+        valid_file_paths = get_valid_paths(driver)
+        latest_file = download_changeset(driver, changeset_downloads, downloaded_ctr)
+        extract_path = clean_extract(latest_file, _id)
+        validate_files(valid_file_paths, extract_path, latest_file.stem)
+        shutil.rmtree(latest_file.stem, ignore_errors=True)
 
 
-def main():
+def validate_files(valid_file_paths, extract_path, downloaded_file_name):
+    downloaded_directory = Path(extract_path, downloaded_file_name)
+    valid_file_paths = {f"{extract_path}/{file_path}" for file_path in valid_file_paths}
+
+    for filename in glob.iglob(f"{downloaded_directory}/**", recursive=True):
+        if not os.path.isfile(filename):
+            continue
+
+        if filename not in valid_file_paths:
+            os.remove(filename)
+
+    delete_empty_folders(downloaded_directory, valid_file_paths)
+
+
+def delete_empty_folders(path, valid_file_paths):
+    for folder in glob.glob(os.path.join(path, "*")):
+        if os.path.isdir(folder):
+            delete_empty_folders(folder, valid_file_paths)
+
+    dir_path = os.listdir(path)
+    if not dir_path and valid_file_paths and path not in valid_file_paths:
+        os.rmdir(path)
+
+
+def init_chrome_config():
     chrome_config, _ = chrome_settings_init()
     changeset_downloads = f"{Path.cwd()}/changesets_download"
     os.makedirs(changeset_downloads, exist_ok=True)
@@ -91,10 +154,21 @@ def main():
         "download.default_directory"
     ] = changeset_downloads
 
-    with webdriver.Chrome(**chrome_config) as driver:
-        login(driver, config.CHANGESET_URL, config.EMAIL, config.PASSWORD)
-        time.sleep(5)
-        scrape_changeset(driver, Path(changeset_downloads))
+    return chrome_config, changeset_downloads
+
+
+def main():
+    chrome_config, changeset_downloads = init_chrome_config()
+
+    try:
+        with webdriver.Chrome(**chrome_config) as driver:
+            login(driver, config.CHANGESET_URL, config.EMAIL, config.PASSWORD)
+            time.sleep(5)
+            scrape_changeset(driver, Path(changeset_downloads))
+    except Exception as e:
+        print(str(e))
+        print(traceback.format_exc())
+    finally:
         shutil.rmtree(Path(changeset_downloads), ignore_errors=True)
 
 
